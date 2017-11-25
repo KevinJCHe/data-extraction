b1ca3233f69b99793630d04fab2c8bfb01431772
Kevin He <kevinjc.he@gmail.com>
2017-11-24 20:02:18 -0500

    Revision Nov 24 2017

--- RecipeDataExtract.py
+++ RecipeDataExtract.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+'''
+@author: Kevin He
+
+Description: Extract recipe data from food2fork API, process the raw data, store it in a database file
+'''
+
 from string import digits
 from string import maketrans
 import sqlite3
@@ -7,22 +13,15 @@ import re
 import urllib2
 import json
 
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-recipe_search = []		#['lobster','chicken','pizza','salmon','spaghetti','omelet', 'macaroni and cheese', 'hamburger', 'curry', 'pasta', 'soup', 'stir fried rice', 'noodles', 'salad', 'sandwich']
-
-#Open database connection
-conn = sqlite3.connect('recipe.db')
-#conn.execute("CREATE TABLE RECIPE (recipe_id text, recipe_title text, recipe_ingredient text, recipe_image_url text);");
-
 def get_recipe_data(recipe_search, conn, header):
 
-	#GET RECIPE ID AND TITLE
 	for r in range(len(recipe_search)):				#GET API KEY FROM FOOD2FORK. ITS FREE
+
+		#Search for the recipes, save the recipe id and recipe title
 		url_search = "http://food2fork.com/api/search?key=YOUR_KEY&q=" + recipe_search[r]
 		response = urllib2.urlopen(urllib2.Request(url_search,headers=header))
 		data = json.loads(response.read())
 
-		#Saves the key values in python variables
 		title = [result['title'] for result in data['recipes']]
 		recipe_id = [result['recipe_id'] for result in data['recipes']]
 
@@ -35,7 +34,7 @@ def get_recipe_data(recipe_search, conn, header):
 				title.remove(title[index])
 				recipe_id.remove(recipe_id[index])
 
-		#GET RECIPE IAMGE URL AND INGREDIENTS
+		#Extract the recipe information using the recipe id
 		for y in range(len(recipe_id)):
 			if y==10:	#Only take ten results per recipe_search item for now
 				break
@@ -44,9 +43,7 @@ def get_recipe_data(recipe_search, conn, header):
 			response_2 = urllib2.urlopen(urllib2.Request(url_get,headers=header))
 			data_2 = json.loads(response_2.read())
 
-			#Saves the key values in python variables
 			ingredients = ' + '.join(data_2['recipe']['ingredients'])
-
 			ingredients = re.sub(u'\xbd', '1/2', ingredients) 
 			ingredients = re.sub(u"(\u2018|\u2019)", "'", ingredients)
 			ingredients = re.sub(u'\xbc', '1/4', ingredients) 
@@ -54,13 +51,14 @@ def get_recipe_data(recipe_search, conn, header):
 			image_url = data_2['recipe']['image_url']
 			only_ing = get_only_ingredient(str(ingredients).split(" + "))
 				
-			#INPUT IT INTO DATABASE
+			#input data into databse
 			conn.execute("INSERT INTO RECIPE (recipe_id, recipe_title, recipe_ingredient, recipe_image_url,only_ingredient) VALUES (?,?,?,?,?)", (recipe_search[r] + str(y), title[y],ingredients,image_url,only_ing));
 
 	conn.commit()	
 
+def process_data(ing_list):
 
-def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(...), not unicode!
+	queries = []
 
 	word_amount =['lb','lbs','pound','pounds','oz','ounce','ounces','kg',
 					'cm','inch',
@@ -82,13 +80,15 @@ def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(..
 					'each','of','up','to','in','if','as','into','for','from','more','the','other','your',
 					'serve','needed','taste','choice','favorite','percent',
 					'thinning','skinless','boneless','free-range','higher-welfare']
-	queries = []
+	
+	get_rid = ["and ","ml ","or ","to ","an ","with "]
 
 	for ingr in ing_list:
 		ing = ingr.lower()
 		
-		if (ing.find("for the ") == 0) or (ing.find(":") == len(ing)):	#For the Salad, Sauce:, Dressing:, etc
-				continue
+		#Skip if line starts with 'For the Salad', 'Sauce:', 'Dressing:', etc
+		if (ing.find("for the ") == 0) or (ing.find(":") == len(ing)):
+			continue
 
 		if "xf1" in ing:
 			ing = re.sub("xf1", "n", ing)			#Replace ñ (xf1) with n
@@ -102,21 +102,26 @@ def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(..
 		if "&nbsp" in ing:
 			ing = re.sub("&nbsp", "", ing)			#Remove &nbsp (&nbsp = " ")
 		
-		ing = ing.strip("-").strip()				#Remove extra spacing and hyphen in front/back of word. Strip spacing after!!
+		ing = ing.strip("-").strip()				#Remove extra spacing and hyphen in front/back of ing. Strip spacing after!!
 		ing = " " + ing + " " + " "					#Add spacing at end for convenience & consistency
 		ing = re.sub("[\(\[].*?[\)\]]", "", ing)	#Remove words inside () [] brackets
 
+		#Remove words that come before a colon :
 		if ': ' in ing:
-			ing = " " + ing.split(': ', 1)[1]							#Remove words that come before a colon :
+			ing = " " + ing.split(': ', 1)[1]	
+		#Remove words that come after a period . A character must come before the period (Exception: oz. lb.)					
 		if '. ' in ing and ing[ing.find(". ")-1] != ' ':	
 			if (ing[ing.find(". ")-1] != 'z' and ing[ing.find(". ")-2] != 'o'):
-				if (ing[ing.find(". ")-1] != 'b' and ing[ing.find(". ")-2] != '1'):
-					ing = ing.split('. ', 1)[0]	+ " " + " "			#Remove words that come after a period . A character must come before the period (Exception: oz. lb.)
+				if (ing[ing.find(". ")-1] != 'b' and ing[ing.find(". ")-2] != 'l'):
+					ing = ing.split('. ', 1)[0]	+ " " + " "		
+		#Remove words that come after ' from a '
 		if ' from a ' in ing:
-			ing = ing.split(' from a ', 1)[0] + " " + " "		#Remove words that come after ' from a '
+			ing = ing.split(' from a ', 1)[0] + " " + " "
+		#Remove words that come after a hyphen - (Exception: 1 - 2 pounds of ...)	
 		if ' - ' in ing and not ing[ing.find(" - ")-1].isdigit():
-			ing = ing.split(' - ', 1)[0] + " " + " "		#Remove words that come after a hyphen - (Exception: 1 - 2 pounds of ...)
+			ing = ing.split(' - ', 1)[0] + " " + " "
 		
+		#Remove any words in the word_amount list
 		if any(x in ing for x in word_amount):	
 			wa = (s for s in word_amount if s in ing) 
 			for s in wa:
@@ -127,13 +132,14 @@ def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(..
 				elif ing[(ing.find(s) + len(s))] == ".":		#e.g. 2 ounces, weight
 					ing = re.sub(' ' + s + '.' + ' ', ' ', ing) #Remove amount words WITH PERIOD IN FRONT,
 				else:
-					ing = re.sub(' ' + s + ' ', ' ', ing) 	#Remove amount words
+					ing = re.sub(' ' + s + ' ', ' ', ing) 		#Remove amount words
 
+		#Remove any words in the word_desc list
 		if any(x in ing for x in word_desc):
 			wd = (t for t in word_desc if t in ing) 
 			for t in wd:
 				if ing[(ing.find(t) - 1)] == " ":		#For efficiency
-					if ing[(ing.find(t) + len(t))] != " ":		#if end of word is not a space
+					if ing[(ing.find(t) + len(t))] != " ":		#if end of ing is not a space
 						if ing[(ing.find(t) + len(t))] == "d":
 							if ing[(ing.find(t) + len(t) + 1)] == " ":			#e.g. slice'd'
 								ing = re.sub(' ' + t + 'd ', ' ', ing) 			#Remove desc words WITH D IN FRONT
@@ -182,35 +188,42 @@ def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(..
 					else:
 						ing = re.sub(' ' + t + ' ', ' ', ing) 			#Remove description words 
 
+		#Remove words that come after a comma ,
 		if ', ' in ing:
-			ing = ing.split(', ', 1)[0] + " " + " "				#Remove words that come after a comma ,
+			ing = ing.split(', ', 1)[0] + " " + " "		
+		#Remove //n at the end		
 		if ing.endswith(r'\n' + " " + " "):
-			ing = ing[:-4]							#Remove that //n at the end							#Remove that //n at the end
+			ing = ing[:-4]							
 		ing = re.sub("[_%/,;()'*~\"+]", '', ing)	#Remove extraneous characters
 		ing = ing.translate(None, digits)			#Remove digits	
 		ing = ing.replace(".", "")					#Remove period cuz re.sub is being stupid
 		ing = ing.replace("\\", "")					#Remove / from string
-		ing = ing.strip().strip("-")				#Remove extra spacing and hyphen in front/back of word. Strip spacing FIRST!!
+		ing = ing.strip().strip("-").strip()		#Remove extra spacing and hyphen in front/back of ing. Strip spacing FIRST!!
 
+		#Remove single characters if it appears in very front of ing
 		if len(ing) > 2:
-			if ing[0].isalpha() and ing[1]== " ": 	#Remove single characters if it appears in very front of word
+			if ing[0].isalpha() and ing[1]== " ": 	
 				ing = ing[1:]
-			if ing[len(ing)-1].isalpha() and ing[len(ing)-2] == " ": 	#Remove single characters if it appears in very end of word
+			#Remove single characters if it appears in very end of ing
+			if ing[len(ing)-1].isalpha() and ing[len(ing)-2] == " ": 	
 				ing = ing[:-2]
 
-		get_rid = ["and ","ml ","or ","to "]
+		#Remove any words in the get_rid list that appear in very front of ing
 		for g in get_rid:
 			if ing.find(g) == 0:								
-				ing = ing[(len(g)-1):]		#e.g. Remove 'and' if it appears in very front of word
+				ing = ing[(len(g)-1):]		#e.g. Remove 'and' if it appears in very front of ing
 
+		#Remove ' a '
 		if ' a ' in ing:	
-			ing = re.sub(' a ', ' ', ing) 	#Remove ' a '
+			ing = re.sub(' a ', ' ', ing) 
+		#Remove ' g '
 		if ' g ' in ing:	
-			ing = re.sub(' g ', ' ', ing) 	#Remove ' g '
-		if ' and ' in ing:					#Split words if they have 'and', such as 'mayo and mustard and ketchup'
+			ing = re.sub(' g ', ' ', ing) 	
+		#Split words if they have 'and', such as 'mayo and mustard and ketchup'
+		if ' and ' in ing:					
 			ing = ing.split(' and ')
 			for a in range(len(ing)):
-				queries.append(ing[a].strip())
+				queries.append((ing[a].strip(),ingr))
 		elif ' with ' in ing:				#Split words if they have 'with', such as 'tomatoes with green chilli'
 			ing = ing.split(' with ')
 			for a in range(len(ing)):
@@ -226,15 +239,25 @@ def get_only_ingredient(ing_list):	#MAKE SURE ing_list is passed along as str(..
 				ing = " ".join(ing.split())
 				queries.append(ing.strip())
 
+	return queries
+
+def get_only_ingredient(ing_list):
+
+	queries = process_data(ing_list)
+
 	queries = " + ".join(queries)
 
 	return queries
 
-
-
-
-
 if __name__ == "__main__":
+
+	#Open database connection
+	conn = sqlite3.connect('recipe.db')
+	#conn.execute("CREATE TABLE RECIPE (recipe_id text, recipe_title text, recipe_ingredient text, recipe_image_url text);");
+	
+	header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+	recipe_search = []		#['lobster','chicken','pizza','salmon','spaghetti','omelet', 'macaroni and cheese', 'hamburger', 'curry', 'pasta', 'soup', 'stir fried rice', 'noodles', 'salad', 'sandwich']
 	
 	get_recipe_data(recipe_search,conn,header)
+	
 	conn.close()
